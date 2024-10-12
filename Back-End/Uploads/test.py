f32c2b84271dd4e73e5af71cb327f057673fee3e
root <root@Richardslaptop>
2024-10-12 14:59:48 -0400

    uploading files. not finished

---
+++ Back-End/Uploads/test.py
@@ -0,0 +1,30 @@
+import json
+from inference_sdk import InferenceHTTPClient
+
+# Initialize the inference client
+CLIENT = InferenceHTTPClient(
+    api_url="https://detect.roboflow.com/",
+    api_key="pEWSO1IdAhzjOJSVVWr4"
+)
+
+# Perform inference on the image
+try:
+    result = CLIENT.infer("upload-3521894298.jpg", model_id="human-fall-e2evv/2")
+
+    # Check if the result is valid and contains predictions
+    if result and isinstance(result, dict):
+        predictions = result.get('predictions', [])
+
+        # Log success and save the result to a JSON file
+        print("Inference successful. Saving results...")
+        print("confidence: ",result["predictions"][0]["confidence"])
+        output_file = "inference_result.json"
+        with open(output_file, "w") as f:
+            json.dump(result, f, indent=4)
+
+        print(f"Results saved to {output_file}")
+    else:
+        print("Inference failed or returned invalid result:", result)
+
+except Exception as e:
+    print(f"Error during inference: {e}")
